55e0b6431d70075584b9ea48c4b197f989c02787
Mercury <parinda.ko@kkumail.com>
2024-09-10 18:18:34 +0700

    updateHeader

--- smart-queue-system/app/page.tsx
+++ smart-queue-system/app/page.tsx
@@ -1,4 +1,4 @@
-import PaymentPage from "./payment/paymentPage";
+// import PaymentPage from "./payment/paymentPage"
 import OrderPayment from "./order/orderPayment";
 export default function Home() {
   return (
