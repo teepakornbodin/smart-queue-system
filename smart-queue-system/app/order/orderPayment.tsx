79bcc838f64d512d769b10baa751a48e198f85bf
Natthiya Chakaew <143895621+Natthiya663040114-3@users.noreply.github.com>
2024-09-10 20:42:00 +0700

    newnana

--- smart-queue-system/app/order/orderPayment.tsx
+++ smart-queue-system/app/order/orderPayment.tsx
@@ -1,65 +1,71 @@
-import pogslide from '../images/pog-slide.png'
-import pog from '../images/pog.png'
-import egg from '../images/eggs.png'
-import mama from '../images/mama.png'
-import mushroom from '../images/mushroom.png'
-import taohu from '../images/taohu.png'
-import lessIcon from '../images/lessIcon.png'
-import plusIcon from '../images/plusIcon.png'
-import cartIcon from '../images/cartIcon.png'
-
+"use client"
+import Image from "next/image";
+import { orderData } from "./orderData";
+import { useState } from "react";
+import lessIcon from '../images/lessIcon.png';
+import plusIcon from '../images/plusIcon.png';
+import cartIcon from '../images/cartIcon.png';
+import cartIcon2 from '../images/cartIcon2.png';
 
 const OrderPayment = () => {
+    const [quantities, setQuantities] = useState(orderData.map(() => 0));
+
+    const handleIncrease = (index: number) => {
+        setQuantities(quantities.map((quantity, i) => i === index ? quantity + 1 : quantity));
+    };
+
+    const handleDecrease = (index: number) => {
+        setQuantities(quantities.map((quantity, i) => i === index && quantity > 0 ? quantity - 1 : quantity));
+    };
+
     return (
         <div className="relative min-h-screen w-full bg-gradient-to-r from-orange-800 to-orange-800 pt-5">
             <h1 className="absolute top-4 left-1/2 transform -translate-x-1/2 text-3xl font-bold text-center text-white">
                 ชาบูเสียบไม้
             </h1>
-            <div className="relative flex items-center justify-center min-h-screen">
-                
-                <div className="bg-white p-20 rounded-lg shadow-lg">
-                    <div className="flex justify-center">
-                        <img src={pogslide.src} alt="pogslide" className="rounded-lg"/>
-                        <div className='flex'>
-                            
-                            <img src={lessIcon.src} alt="lessIcon" className="rounded-lg" />
-                            <img src={plusIcon.src} alt="plusIcon" className="rounded-lg" />
-                            <img src={cartIcon.src} alt="cartIcon" className="rounded-lg" />
+            <div>
+                <div className="bg-white p-5 rounded-3xl shadow-lg mt-11">
+                    <div className="grid grid-cols-2 md:grid-cols-2 gap-5 ">
+                        {orderData.map((data, index) => (
+                            <div key={index} className="">
+                                <div className="items-center">
+                                    <Image src={data.image} alt="product" width={350} height={350} />
+                                    <div className="ml-5">
+                                        <h1 className="text-lg font-bold text-center mt-2">{data.name}</h1>
+                                        <h1 className="text-lg font-bold text-center">{quantities[index] * 10}฿</h1>
+                                        <div className="flex items-center justify-center mt-2">
+                                            <button onClick={() => handleDecrease(index)}>
+                                                <Image src={lessIcon} alt="less" width={30} height={30} />
+                                            </button>
+                                            <h1 className="text-lg font-bold text-center mx-7">{quantities[index]}</h1>
+                                            <button onClick={() => handleIncrease(index)}>
+                                                <Image src={plusIcon} alt="plus" width={30} height={30} />
+                                            </button>
+                                        </div>
+
+                                    </div>
+
+                                </div>
 
 
+                            </div>
+
+                        ))}
+                        <h1 className="text-lg font-bold text-center mt-6 flex" >
+                        <Image src={cartIcon} alt="cart" width={30} height={30} /> 
+                            Total: ฿{quantities.reduce((total, quantity) => total + quantity * 10, 0)}
+                        </h1>
+
+                       
+                            <button className="w-full bg-orange-800 text-white p-2 rounded-md hover:bg-orange-700 transition duration-300">
+                                ยืนยันรายการสินค้า
+                            </button>
+                        </div>
                     </div>
-                    <img src={pog.src} alt="pog" className="w-40 h-40 object-cover rounded-lg" />
-                </div>
-
-                <div className="flex justify-center">
-                    <img src={egg.src} alt="egg" className="w-40 h-40 object-cover rounded-lg" />
-                    <img src={mama.src} alt="mama" className="w-40 h-40 object-cover rounded-lg" />
-
-                </div>
-
-                <div className="flex justify-center">
-                    <img src={mushroom.src} alt="mushroom" className="w-40 h-40 object-cover rounded-lg" />
-                    <img src={taohu.src} alt="taohu" className="w-40 h-40 object-cover rounded-lg" />
-                </div>
-
-                <div className="mt-5">
-                    <h1 className="text-2xl font-bold text-center">
-                        ชาบูเสียบไม้
-                    </h1>
-                    <p className="text-center">
-                        ราคา 299 บาท
-                    </p>
-                </div>
-
-                <div className="mt-5">
-                    <button className="w-full bg-orange-800 text-white p-2 rounded-md">
-                        ชำระเงิน
-                    </button>
                 </div>
             </div>
-        </div>
-            </div >
-            );
+
+    );
 };
 
 export default OrderPayment;
--- smart-queue-system/app/page.tsx
+++ smart-queue-system/app/page.tsx
@@ -1,4 +1,3 @@
-import PaymentPage from "./payment/paymentPage";
 import OrderPayment from "./order/orderPayment";
 export default function Home() {
   return (
