5f2a2632323cde7bb0f2aa9f1016131406d93bc4
Suwitthawas <Suwitthawas.h@kkumail.com>
2024-09-10 17:47:51 +0700

    upforpaymentpage

--- smart-queue-system/app/pages/paymentPage.tsx
+++ smart-queue-system/app/pages/paymentPage.tsx
@@ -2,8 +2,12 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import qrImage from "../images/qrCode.png";
+import { FiArrowLeft } from 'react-icons/fi';
+
 
 const PaymentPage: React.FC = () => {
+  // const [price, setPrice] = useState([])
+  const price = 50;
     
     // useEffect(() => {
     //     // ตั้งเวลา 5 นาที (300,000 มิลลิวินาที)
@@ -13,43 +17,50 @@ const PaymentPage: React.FC = () => {
     //     // ล้าง Timer เมื่อ Component ถูก unmount
     //     return () => clearTimeout(timer);
     //   }, [router]);
-    
+    //CA7257 color
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="min-h-screen grid items-center justify-center bg-gray-100">
       <div className="bg-white shadow-md rounded-lg p-6 max-w-md w-full">
+        
+        <div className='bg-[#CA7257] rounded-lg relative flex justify-center p-1.5 mt-1.5'>
+          <button className='absolute left-2'><FiArrowLeft color='white'/></button>
+          <div>
+            <p className='text-white text-xs'>ชำระเงิน</p>
+          </div>
+        </div>
         {/* QR Code Section */}
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center m-1">
           {/* Placeholder for QR Code Image */}
           <Image 
             src={qrImage} 
             alt="QR Code" 
-            width={192} // กำหนดขนาดที่ต้องการ
-            height={192} 
-            className="object-contain mb-4"
+            width={185} // กำหนดขนาดที่ต้องการ
+            height={185} 
+            className="object-contain mb-4 border-1 scale-[80%]"
           />
-          <p className="text-center text-gray-600">
-            สแกน QR เพื่อโอนเข้าบัญชี<br />
-            ชื่อ: น.ส. ปริญดา โคตะนิวงษ์<br />
-            บัญชี: xxx-x-x6406-x<br />
-            เลขที่อ้างอิง: 004999122325136
-          </p>
+          <div className='bg-[#045188] border-1 border-red-200 rounded-lg'>
+            <p className=" text-center text-white text-xs px-4 py-2">
+              สแกน QR เพื่อชำระเงิน<br />
+              ยอดชำระ {price} บาท
+            </p>
+          </div>
         </div>
+
+        {/* Timer Message */}
+      <p className="mt-4 text-red-500 text-center text-xs">
+        กรุณาชำระเงินภายใน 5 นาที
+      </p>
         
         {/* Buttons */}
         <div className="mt-6 flex justify-between">
-          <button className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600">
+          <button className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 text-xs">
             ยกเลิก
           </button>
-          <button className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600">
+          <button className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 text-xs">
             ตกลง
           </button>
         </div>
       </div>
-
-      {/* Timer Message */}
-      <p className="mt-4 text-red-500 text-center">
-        กรุณาชำระเงินภายใน 5 นาที
-      </p>
     </div>
   );
 };
