d6ffec23e87e5a6a3061bdb76cb3276991318023
Mercury <parinda.ko@kkumail.com>
2024-09-10 18:12:12 +0700

    updateHeaderPage

--- smart-queue-system/app/pages/headerPage.tsx
+++ smart-queue-system/app/pages/headerPage.tsx
@@ -1,58 +1,47 @@
 // pages/index.js
+import Link from 'next/link';
 import Image from 'next/image';
-import Image1 from '../images/food2.png';
-import Image2 from '../images/food3.png';
-import Image3 from '../images/food1.png';
+import bg from '../images/bgHeader.svg';
+import intro from '../images/intro.svg';
+import User from '../images/user.svg';
 
 const headerPage = () => {
   return (
-    <div className="h-screen bg-white flex flex-col justify-between items-center p-4">
-      <h1 className="text-4xl font-bold mt-11">EN Smart Queue</h1>
-      <div className="flex-grow flex justify-center items-center">
-        <input
-          type="text"
-          placeholder="พิมพ์ชื่อของคุณ......"
-          className="bg-white text-black px-4 py-2 rounded-full text-center shadow-lg border-2 border-black"
-        />
-      </div>
-      
-      <div className="mb-10">
-        <button className="bg-black text-white px-6 py-2 rounded-full shadow-lg">
-          <h2>ยืนยัน</h2>
-        </button>
-      </div>
-
-      <div className="flex justify-between w-full px-10 mb-4">
-        <div className="relative w-32 h-32">
+    <div
+      className="h-screen bg-cover bg-center flex flex-col p-4 w-full relative"
+      style={{ backgroundImage: `url(${bg.src})`,
+              }}
+    >
+       <div className="mt-10 ml-6">
           <Image
-            src={Image1} // path ของรูปอาหารที่ 1
-            alt="Food 1"
-            // layout="fill"
-            objectFit="cover"
-            className="rounded-full"
+            src={intro}
+            alt="intro"
           />
-        </div>
+       </div>
+       <div className="flex flex-col justify-between items-center" style={{marginTop:'17.5rem'}}>
+          <div className='mb-20'>
+            <Image
+            src={User}
+            alt="User"
+          />
+          </div>
+          
+          <div className="px-4 mb-10">
+          <input
+            type="text"
+            placeholder="พิมพ์ชื่อของคุณ......"
+            className="bg-white text-black px-10 py-4 rounded-full text-center shadow-lg border-2 border-[#CA7257] w-full max-w-md text-xl font-semibold"
+          />
+          </div>
 
-        <div className="relative w-32 h-32">
-          <Image
-            src={Image2} // path ของรูปอาหารที่ 2
-            alt="Food 2"
-            layout="fill"
-            objectFit="cover"
-            className="rounded-full"
-          />
-        </div>
-
-        <div className="relative w-32 h-32">
-          <Image
-            src={Image3} // ใช้รูปจากการ import มา
-            alt="Food 3"
-            layout="fill"
-            objectFit="cover"
-            className="rounded-full"
-          />
-        </div>
-      </div>
+          <div className="bt-10">
+            <Link href={""}>
+            <button className="bg-[#CA7257] text-white px-6 py-3 rounded-full shadow-lg">
+              <h2 className="text-lg font-semibold">ยืนยัน</h2>
+            </button>
+            </Link>
+          </div>
+       </div>
     </div>
   );
 }
