c63b8fbd5f3ff8dca08002a33d47dd7283f62e17
ikanthi258 <kanthi.p@kkumail.com>
2024-09-10 22:19:34 +0700

    QPtest

--- smart-queue-system/app/page.tsx
+++ smart-queue-system/app/page.tsx
@@ -1,4 +1,4 @@
-import HeaderPage from "./pages/headerPage";
+import HeaderPage from "./pages/queuePage";
 
 export default function Home() {
   return (
--- smart-queue-system/app/pages/queuePage.tsx
+++ smart-queue-system/app/pages/queuePage.tsx
@@ -2,48 +2,66 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import waitImgame from "../images/queue1.png";
+import locationImgame from "../images/location_logo.png";
 
 const PaymentPage: React.FC = () => {
-
-    // useEffect(() => {
-    //     // ตั้งเวลา 5 นาที (300,000 มิลลิวินาที)
-    //     const timer = setTimeout(() => {
-    //       router.push('/order'); // กลับไปที่หน้าชื่อว่า Order
-    //     }, 300000);
-    //     // ล้าง Timer เมื่อ Component ถูก unmount
-    //     return () => clearTimeout(timer);
-    //   }, [router]);
-
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-            <h1 className="text-4xl text-black font-bold mt-11">Queue </h1>
-            <div className="bg-white shadow-md rounded-lg p-6 max-w-md w-full">
-                {/* Queue Text Section */}
-                <div className="flex flex-col items-center">
-                    <p className="text-black">
-                        th <br />
-                    </p>
-                    <p className="text-center text-gray-900 text-9xl">
-                        025 <br /> 
-                    </p>
-                </div>
-            </div>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-[#CA7257]">
+            <h1 className="text-6xl text-white-900 font-bold mt-11">Queue </h1>
+            <div className="bg-white shadow-md rounded-lg -md w-full">
 
-            <div className="bg-white shadow-md rounded-lg p-8 max-w-md w-full">
+                {/* Queue Text Section */}
+                <div className="mt-6 flex p-2 justify-center">
+                    <div className="mt-6 flex justify-end">
+                        <p className="text-center text-[#CA7257] text-9xl">
+                            025 <br />
+                        </p>
+                        <p className="text-[#CA7257] text-3xl">
+                            th <br />
+                        </p>
+                    </div>
+                </div>
+
+                {/* Text Section */}
+                <div className="flex flex-col items-center">
+                    <p className="text-[#CA7257] p-4 text-2xl">
+                        POSITION IN QUEUE <br />
+                    </p>
+                    <p className="text-[#CA7257] max-w front-bold text-3xl">
+                        ....................................................<br />
+                    </p>
+
+                    <div className="mt-6 flex justify-between">
+                        {/* Placeholder for Image */}
+                        <Image
+                            src={locationImgame}
+                            alt="Waiting Image"
+                            width={30} // กำหนดขนาดที่ต้องการ
+                            height={30}
+                            className="object-contain"
+                        />
+                        <p className="text-center text-[#CA7257] text-2xl">
+                            ตลาดน้อยตึกเพียรวิจิตร<br/>
+                        </p>
+                    </div>
+                </div>
+
                 {/* Image Section */}
                 <div className="flex flex-col items-center">
+
                     {/* Placeholder for Image */}
                     <Image
                         src={waitImgame}
                         alt="Waiting Image"
-                        width={192} // กำหนดขนาดที่ต้องการ
-                        height={192}
-                        className="object-contain mb-4"
+                        width={320} // กำหนดขนาดที่ต้องการ
+                        height={320}
+                        className="object-contain"
                     />
-                    <p className="text-center text-gray-900 text-3xl">
+                    <p className="text-center text-gray-900 font-bold text-3xl">
                         Please wait ...<br />
                     </p>
                 </div>
+
             </div>
         </div>
     );
