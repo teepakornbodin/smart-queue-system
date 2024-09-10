ac85bbbaec9291235becf5233b425218a935dafe
Suwitthawas <Suwitthawas.h@kkumail.com>
2024-09-10 18:37:29 +0700

    update for page

--- smart-queue-system/app/page.tsx
+++ smart-queue-system/app/page.tsx
@@ -1,4 +1,4 @@
-import PaymentPage from "./payment/paymentPage";
+import PaymentPage from "./pages/paymentPage";
 import OrderPayment from "./order/orderPayment";
 export default function Home() {
   return (
