0ca9d71b9ce95cb4defa047ba5994b105ae53cae
ikanthi258 <kanthi.p@kkumail.com>
2024-09-12 13:26:11 +0700

    ordinal number

--- smart-queue-system/app/queue/page.tsx
+++ smart-queue-system/app/queue/page.tsx
@@ -1,13 +1,51 @@
-// pages/payment.tsx
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import waitImgame from '../images/queue1.png';
 import locationImgame from "../images/location_logo.png";
 
 
-const PaymentPage: React.FC = () => {
+const QueuePage: React.FC = () => {
+    const [queue, setQueue] = useState(1);
+    const [ordinalNum, setOrdinalNum] = useState('');
     const currentDate = new Date().toLocaleDateString('th-TH', { timeZone: 'Asia/Bangkok' });
     const currentTime = new Date().toLocaleTimeString('th-TH', { timeZone: 'Asia/Bangkok' });
+
+    // ฟังก์ชันสำหรับเพิ่มค่า queue
+    const incrementQueue = () => {
+        setQueue(queue + 1);  // เพิ่มค่า queue ขึ้น 1
+    };
+
+    // ฟังก์ชันสำหรับลดค่า queue
+    const decrementQueue = () => {
+        setQueue(queue - 1);  // ลดค่า queue ลง 1
+    };
+
+    // ฟังก์ชันสำหรับรีเซ็ตค่า queue
+    const resetQueue = () => {
+        setQueue(1);  // ตั้งค่า queue กลับไปที่ 0
+    };
+
+    // ฟังก์ชันสำหรับเซ็ตค่า Ordinal number
+    function getOrdinalSuffix(number: number): string {
+        const remainder10 = number % 10;
+        const remainder100 = number % 100;
+        if (remainder100 >= 11 && remainder100 <= 13) {
+            return `th`;  // เลขที่ลงท้ายด้วย 11, 12, 13 ใช้ th เสมอ
+        }
+
+        switch (remainder10) {
+            case 1:
+                return `st`;  // เลขที่ลงท้ายด้วย 1 ใช้ st
+            case 2:
+                return `nd`;  // เลขที่ลงท้ายด้วย 2 ใช้ nd
+            case 3:
+                return `rd`;  // เลขที่ลงท้ายด้วย 3 ใช้ rd
+            default:
+                return `th`;  // เลขอื่น ๆ ใช้ th
+        }
+    }
 
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-[#CA7257]">
@@ -18,10 +56,10 @@ const PaymentPage: React.FC = () => {
                 <div className="mt-6 flex p-2 justify-center">
                     <div className="mt-6 flex justify-end">
                         <p className="text-center text-[#CA7257] text-9xl">
-                            025 <br />
+                            {queue} <br />
                         </p>
                         <p className="text-[#CA7257] text-3xl">
-                            th <br />
+                            {getOrdinalSuffix(queue)} <br />
                         </p>
                     </div>
                 </div>
@@ -71,4 +109,4 @@ const PaymentPage: React.FC = () => {
     );
 };
 
-export default PaymentPage;
+export default QueuePage;
