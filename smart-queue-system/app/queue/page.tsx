5b42e90fcf1e8d9807ce138ccf9da3cbc419ab5a
ikanthi258 <kanthi.p@kkumail.com>
2024-09-12 14:12:43 +0700

    edited layout

--- smart-queue-system/app/queue/page.tsx
+++ smart-queue-system/app/queue/page.tsx
@@ -7,45 +7,23 @@ import locationImgame from "../images/location_logo.png";
 
 
 const QueuePage: React.FC = () => {
-    const [queue, setQueue] = useState(1);
-    const [ordinalNum, setOrdinalNum] = useState('');
+    const [userQueue, setUserQueue] = useState(2);
+    const [currentQueue, setCurrentQueue] = useState(2);
+
     const currentDate = new Date().toLocaleDateString('th-TH', { timeZone: 'Asia/Bangkok' });
-    const currentTime = new Date().toLocaleTimeString('th-TH', { timeZone: 'Asia/Bangkok' });
 
-    // ฟังก์ชันสำหรับเพิ่มค่า queue
-    const incrementQueue = () => {
-        setQueue(queue + 1);  // เพิ่มค่า queue ขึ้น 1
+    // ฟังก์ชันสำหรับเพิ่มค่า current queue
+    const incrementCurrentQueue = () => {
+        setCurrentQueue(currentQueue + 1);  // เพิ่มค่า current queue  ขึ้น 1
     };
-
-    // ฟังก์ชันสำหรับลดค่า queue
-    const decrementQueue = () => {
-        setQueue(queue - 1);  // ลดค่า queue ลง 1
+    // ฟังก์ชันสำหรับลดค่า current queue
+    const decrementCurrentQueue = () => {
+        setCurrentQueue(currentQueue - 1);  // ลดค่า current queue  ลง 1
     };
-
-    // ฟังก์ชันสำหรับรีเซ็ตค่า queue
-    const resetQueue = () => {
-        setQueue(1);  // ตั้งค่า queue กลับไปที่ 0
+    // ฟังก์ชันสำหรับรีเซ็ตค่า current queue
+    const resetCurrentQueue = () => {
+        setCurrentQueue(1);  // ตั้งค่า current queue กลับไปที่ 1
     };
-
-    // ฟังก์ชันสำหรับเซ็ตค่า Ordinal number
-    function getOrdinalSuffix(number: number): string {
-        const remainder10 = number % 10;
-        const remainder100 = number % 100;
-        if (remainder100 >= 11 && remainder100 <= 13) {
-            return `th`;  // เลขที่ลงท้ายด้วย 11, 12, 13 ใช้ th เสมอ
-        }
-
-        switch (remainder10) {
-            case 1:
-                return `st`;  // เลขที่ลงท้ายด้วย 1 ใช้ st
-            case 2:
-                return `nd`;  // เลขที่ลงท้ายด้วย 2 ใช้ nd
-            case 3:
-                return `rd`;  // เลขที่ลงท้ายด้วย 3 ใช้ rd
-            default:
-                return `th`;  // เลขอื่น ๆ ใช้ th
-        }
-    }
 
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-[#CA7257]">
@@ -53,13 +31,13 @@ const QueuePage: React.FC = () => {
             <div className="bg-white shadow-md rounded-lg max-w-md w-full">
 
                 {/* Queue Text Section */}
-                <div className="mt-6 flex p-2 justify-center">
-                    <div className="mt-6 flex justify-end">
-                        <p className="text-center text-[#CA7257] text-9xl">
-                            {queue} <br />
+                <div className="mt-3 block p-2 justify-center">
+                    <div className=" block justify-end">
+                        <p className="text-center text-[#CA7257] text-9xl ml-7">
+                            test <br />
                         </p>
-                        <p className="text-[#CA7257] text-3xl">
-                            {getOrdinalSuffix(queue)} <br />
+                        <p className="text-center text-[#CA7257] text-3xl">
+                            test <br />
                         </p>
                     </div>
                 </div>
@@ -70,17 +48,17 @@ const QueuePage: React.FC = () => {
                         POSITION IN QUEUE <br />
                     </p>
                     <p className="text-black font-light text-xl">
-                        {currentDate} {currentTime} <br />
+                        {currentDate} <br />
                     </p>
 
-                    <div className="mt-6 flex justify-between">
+                    <div className="mt-6 flex justify-between items-center">
                         {/* Placeholder for Image */}
                         <Image
                             src={locationImgame}
                             alt="Waiting Image"
                             width={30} // กำหนดขนาดที่ต้องการ
                             height={30}
-                            className="object-contain"
+                            className="object-contain mr-2"
                         />
                         <p className="text-[#CA7257] font-light text-2xl">
                             ตลาดน้อยตึกเพียรวิจิตร<br />
