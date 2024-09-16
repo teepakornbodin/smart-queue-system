19f907d25c1946ee61e05f16bf37fa402dd85c73
ChaiwatBoonsri <Chaiwat.bo@kkumail.com>
2024-09-16 14:52:16 +0700

    install mongoose

--- smart-queue-system/app/page.tsx
+++ smart-queue-system/app/page.tsx
@@ -1,6 +1,8 @@
 import HeaderPage from "./queue/page";
+import connectDB from "../config/database"
 
 export default function Home() {
+  connectDB(); // test DB connection
   return (
     <div>
       <HeaderPage />
