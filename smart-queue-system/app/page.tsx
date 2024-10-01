637033b9613c9feeeeb41ee893e97ee7d6bbd471
Mercury <parinda.ko@kkumail.com>
2024-10-01 10:14:41 +0700

    update

--- smart-queue-system/app/page.tsx
+++ smart-queue-system/app/page.tsx
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-// import HeaderPage from "./queue/page";
-import home1 from './Home/page'
-import PaymentPage from './payment/page';
-import HeaderPage from './Home/page';
-=======
 import AdminPage from "./Admin/page";
->>>>>>> parinda
 
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
-      <HeaderPage/>
-=======
       <AdminPage />
->>>>>>> parinda
     </div>
   );
 }
