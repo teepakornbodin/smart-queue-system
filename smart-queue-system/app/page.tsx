15573be48538f97bdbad151d3156c472ab038398
Natthiya Chakaew <143895621+Natthiya663040114-3@users.noreply.github.com>
2024-10-07 17:13:19 +0700

    nana

--- smart-queue-system/app/page.tsx
+++ smart-queue-system/app/page.tsx
@@ -1,11 +1,12 @@
 import HeaderPage from "../queue/page";
 import PaymentPage from "./Payment/page";
-
+import HomePage from "./Home/page";
 export default function Home() {
   return (
     <div>
       {/* <HeaderPage /> */}
-      <PaymentPage/>
+      {/* <PaymentPage/> */}
+      <HomePage/>
     </div>
   );
 }
